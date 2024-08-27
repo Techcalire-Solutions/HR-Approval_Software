2644fe29636392e6fec781b24187b03de06ba978
Amina123-creator <80471446+Amina123-creator@users.noreply.github.com>
2024-08-27 21:52:08 +0400

    view approval

---
+++ Frontend/src/app/pages/add-approval/view-approval/verification-dialogue/verification-dialogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-dialogue',
+  standalone: true,
+  imports: [],
+  templateUrl: './verification-dialogue.component.html',
+  styleUrl: './verification-dialogue.component.scss'
+})
+export class VerificationDialogueComponent {
+
+}
--- Frontend/src/app/theme/components/sidenav/sidenav.component.ts
+++ Frontend/src/app/theme/components/sidenav/sidenav.component.ts
@@ -13,6 +13,7 @@ import { User } from '../../../common/models/user.model';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../../../services/users.service';
 import { InvoiceService } from '@services/invoice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -49,7 +50,7 @@ export class SidenavComponent implements OnInit , PipeTransform{
   public userImage = 'img/users/user.jpg';
   public menuItems: Array<any>;
   public settings: Settings;
-  constructor(public settingsService: SettingsService,public invoiceService:InvoiceService, public menuService: MenuService){
+  constructor(private router: Router, public settingsService: SettingsService,public invoiceService:InvoiceService, public menuService: MenuService){
       this.settings = this.settingsService.settings;
   }
 user:any
@@ -70,7 +71,30 @@ role: String
     })
   }
   }
+  logout() {
+    // Clear authentication tokens or session data here
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('JWT_TOKEN');
+    localStorage.removeItem('REFRESH_TOKEN');
+    sessionStorage.clear(); // Clear all session storage if needed
+    this.router.navigate(['/']);
+  }
+  // logout(){
+  //   const dialogRef = this.dialog.open(LogoutComponent, {
+  //     width: '440px',
+  //     data: {
+  //       id: this.userId,
+  //       role: this.userRole
+  //     }
+  //   });
 
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     if (result === true) {
+  //       this.router.navigateByUrl('');
+  //     }
+  //   });
+  // }
 
   public closeSubMenus(){
     let menu = document.getElementById("vertical-menu");
