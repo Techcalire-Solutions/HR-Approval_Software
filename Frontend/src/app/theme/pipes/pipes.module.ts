4058af68bd00c85b0884788f8d0dba29aa9a5372
“nishidaps” <chinchoos11@gmail.com>
2024-09-09 20:16:07 +0530

    user completed

--- Frontend/src/app/theme/pipes/pipes.module.ts
+++ Frontend/src/app/theme/pipes/pipes.module.ts
@@ -3,30 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { PaginationPipe} from './pagination/pagination.pipe';
 import { ProfilePicturePipe } from './profilePicture/profilePicture.pipe';
-import { ChatPersonSearchPipe } from './search/chat-person-search.pipe';
-import { UserSearchPipe } from './search/user-search.pipe';
 import { TruncatePipe } from './truncate/truncate.pipe';
-import { MailSearchPipe } from './search/mail-search.pipe';
 
 @NgModule({
-    imports: [ 
-        CommonModule 
+    imports: [
+        CommonModule
     ],
     declarations: [
         PaginationPipe,
         ProfilePicturePipe,
-        ChatPersonSearchPipe,
-        UserSearchPipe,
         TruncatePipe,
-        MailSearchPipe
     ],
     exports: [
         PaginationPipe,
         ProfilePicturePipe,
-        ChatPersonSearchPipe,
-        UserSearchPipe,
-        TruncatePipe,
-        MailSearchPipe
+        TruncatePipe
     ]
 })
 export class PipesModule { }
