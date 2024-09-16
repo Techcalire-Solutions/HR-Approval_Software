9f1b5f47359f4590da6856123ebe8abf276d939a
Anupama-vellilath <anupamav08@gmail.com>
2024-09-16 18:58:45 +0530

    Anu develop changes

--- Backend/leave/routers/leave.js
+++ Backend/leave/routers/leave.js
@@ -70,56 +70,9 @@ router.put('/:leaveId/status', authenticateToken, async (req, res) => {
     res.status(500).send(error.message);
   }
 });
-const authenticateToken = require('../../middleware/authorization')
-const sequelize = require('../../utils/db')
-const { Op, fn, col, where } = require('sequelize');
-const Leave = require('../models/leave')
 
 
-router.post('/', authenticateToken,async(req,res)=>{
-    try {
-        const {
-          
-            leaveTypeId,
-            startDate,
-            endDate,
-            noOfDays,
-            notes,
-            status
-        } = req.body;
-        const userId = req.user.id; // added by Amina 
-        const leave = new Leave({
-            userId,
-            leaveTypeId,
-            startDate,
-            endDate,
-            noOfDays,
-            notes,
-            status
 
-        });
-        await leave.save();
-        res.send(leave)
-
-        
-    } catch (error) {
-        res.send(error.message)
-        
-    }
-})
-
-
-router.get('/',authenticateToken, async(req,res)=>{
-    try {
-        const leave = await Leave.findAll({})
-        res.send(leave)
-        
-    } catch (error) {
-        res.send(error.message)
-        
-    }
-
-})
 
 
 
