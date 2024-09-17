8ff9a5e1c8beaa0b54dd47fae5796ab6f3543a17
Anupama-vellilath <anupamav08@gmail.com>
2024-09-17 23:44:44 +0530

    Anu develop changes

--- Backend/leave/models/leave.js
+++ Backend/leave/models/leave.js
@@ -2,14 +2,13 @@ const {DataTypes} =  require('sequelize')
 const sequelize = require('../../utils/db')
 
 const Leave = sequelize.define('leave',{
-    userId: {type : DataTypes.INTEGER, allowNull : true},
+    userId: {type : DataTypes.INTEGER, allowNull : false},
     leaveTypeId : {type : DataTypes.INTEGER},
     startDate : {type : DataTypes.DATE, allowNull : true},
     endDate : {type : DataTypes.DATE, allowNull : true},
     noOfDays : {type : DataTypes.INTEGER},
     notes : { type : DataTypes.STRING, allowNull : true },
     status : { type : DataTypes.STRING, allowNull : true },
-
     halfDay: { type : DataTypes.BOOLEAN,default:false},
     halfDayTime: { type : DataTypes.STRING, allowNull : true },
     
