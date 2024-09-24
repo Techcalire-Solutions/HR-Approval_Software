1816cc98efe7a59e702bc410edc7f74d2032bce9
Amina123-creator <80471446+Amina123-creator@users.noreply.github.com>
2024-09-24 17:22:49 +0530

    TEAM SET

--- Backend/utils/association.js
+++ Backend/utils/association.js
@@ -9,6 +9,8 @@ const User = require('../users/models/user');
 
 async function syncModel() {
     await sequelize.sync({alter: true})
+
+ 
     
 
     const roleData = [
@@ -33,9 +35,9 @@ async function syncModel() {
         { name: "Sameer", email: "sameer@gmail.com", phoneNumber:"9846335570", password: "sameer@123", roleId: 1,teamId:1, status: true },
         { name: "Vishnu", email: "vishnu@gmail.com", phoneNumber:"9846335123", password: "vishnu@123", roleId: 1, teamId:1, status: true },
         { name: "Sijin", email: "sijin@gmail.com", phoneNumber:"9846442233", password: "sijin@123", roleId: 2, teamId:1, status: true },
-        { name: "Shibin", email: "shibin@gmail.com", phoneNumber:"9847391646", password: "shibin@123", roleId: 3, teamId:1, status: true },
-        { name: "Fawas", email: "fawas@gmail.com", phoneNumber:"98667799551", password: "fawas@123", roleId: 4, teamId:1, status: true },
-        { name: "Admin", email: "admin@gmail.com", phoneNumber:"1234567890", password: "admin@123", roleId: 6, teamId:1, status: true },
+        { name: "Shibin", email: "shibin@gmail.com", phoneNumber:"9847391646", password: "shibin@123", roleId: 3, status: true },
+        { name: "Fawas", email: "fawas@gmail.com", phoneNumber:"98667799551", password: "fawas@123", roleId: 4, status: true },
+        { name: "Admin", email: "admin@gmail.com", phoneNumber:"1234567890", password: "admin@123", roleId: 6, status: true },
         { name: "Dhanalakshmi", email: "dhanalakshmi@gmail.com", phoneNumber:"1234567890", password: "dhanalakshmi@123", roleId: 7, teamId:1, status: true },
     ];
     const user = await User.findAll({});
@@ -51,31 +53,31 @@ async function syncModel() {
         }
     }
 
-    // const team = await Team.findAll({});
+    const team = await Team.findAll({});
 
-    // if (team.length === 0) {
-    //     try {
-    //         await Team.bulkCreate([
-    //             { teamName: "EMEA", userId: 1 },
-    //         ]);
+    if (team.length === 0) {
+        try {
+            await Team.bulkCreate([
+                { teamName: "EMEA", userId: 1 },
+            ]);
     
-    //         const teams = await Team.findAll();
-    //         const teamMembers = [
-    //             { teamId: 1, userId: 1 },
-    //             { teamId: 1, userId: 2 },
-    //             { teamId: 1, userId: 3 },
-    //         ];
+            const teams = await Team.findAll();
+            const teamMembers = [
+                { teamId: 1, userId: 1 },
+                { teamId: 1, userId: 2 },
+                { teamId: 1, userId: 3 },
+            ];
     
-    //         for (const team of teams) {
-    //             await TeamMember.bulkCreate(teamMembers.map(member => ({
-    //                 ...member,
-    //                 teamId: team.id,
-    //             })));
-    //         }
-    //     } catch (error) {
-    //         console.error("Error creating team and team members:", error.message);
-    //     }
-    // }
+            for (const team of teams) {
+                await TeamMember.bulkCreate(teamMembers.map(member => ({
+                    ...member,
+                    teamId: team.id,
+                })));
+            }
+        } catch (error) {
+            console.error("Error creating team and team members:", error.message);
+        }
+    }
 }
 
 module.exports = syncModel;
