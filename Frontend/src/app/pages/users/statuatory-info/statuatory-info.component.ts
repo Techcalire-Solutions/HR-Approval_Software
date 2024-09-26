3588eb3c27fbe0cdc4069a89c06474dd7ffa618e
NishidaOAC <nishida@onboardaero.com>
2024-09-26 15:27:39 +0530

    amina correctiuons

--- Frontend/src/app/pages/users/statuatory-info/statuatory-info.component.ts
+++ Frontend/src/app/pages/users/statuatory-info/statuatory-info.component.ts
@@ -64,6 +64,8 @@ export class StatuatoryInfoComponent implements OnDestroy {
   @Output() dataSubmitted = new EventEmitter<any>();
   submitSub!: Subscription;
   onSubmit(){
+    console.log(this.editStatus);
+    
     let submit = {
       ...this.form.getRawValue()
     }
