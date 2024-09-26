087990d54d889fb1256460fad00df797397cdd13
anupama-oac <anupama@onboardaero.com>
2024-09-26 18:15:18 +0530

    Dashboard / nav / super admin

--- Backend/utils/association.js
+++ Backend/utils/association.js
@@ -8,7 +8,7 @@ const User = require('../users/models/user');
 
 
 async function syncModel() {
-    await sequelize.sync({force: true})
+    await sequelize.sync({alter: true})
 
     // Team.belongsTo(User, {
     //     foreignKey: "userId",
@@ -44,13 +44,13 @@ async function syncModel() {
         { name: "Ashbin", email: "ashbin@gmail.com", phoneNumber:"9846335577", password: "ashbin@123", roleId: 1, teamId:1,status: true, empNo: 'OAC-2024-001' },
         { name: "Sameer", email: "sameer@gmail.com", phoneNumber:"9846335570", password: "sameer@123", roleId: 1,teamId:1, status: true, empNo: 'OAC-2024-002' },
         { name: "Vishnu", email: "vishnu@gmail.com", phoneNumber:"9846335123", password: "vishnu@123", roleId: 1, teamId:1, status: true, empNo: 'OAC-2024-003' },
-        { name: "Sijin", email: "sijin@gmail.com", phoneNumber:"9846442233", password: "sijin@123", roleId: 2, status: true, empNo: 'OAC-2024-004', reportingManager: true },
+        { name: "Sijin", email: "anupama@onboardaero.com", phoneNumber:"9846442233", password: "sijin@123", roleId: 2, status: true, empNo: 'OAC-2024-004', reportingManager: true },
         { name: "Shibin", email: "shibin@gmail.com", phoneNumber:"9847391646", password: "shibin@123", roleId: 3, status: true, empNo: 'OAC-2024-005' },
         { name: "Fawas", email: "fawas@gmail.com", phoneNumber:"98667799551", password: "fawas@123", roleId: 4, status: true, empNo: 'OAC-2024-006' },
         { name: "Approval Admin", email: "admin@gmail.com", phoneNumber:"1234567890", password: "admin@123", roleId: 6, status: true, empNo: 'OAC-2024-007' },
         { name: "HR Admin", email:"hradmin@gmail.com", phoneNumber:"1234567890", password: "hradmin@123", roleId: 7, status: true, empNo: 'OAC-2024-008'},
-        { name: "Dhanalakshmi", email: "dhanalakshmi@gmail.com", phoneNumber:"1234567890", password: "dhanalakshmi@123", roleId: 8, status: true, empNo: 'OAC-2024-008' },
         { name: "Super Admin", email: "superadmin@gmail.com", phoneNumber:"1234567890", password: "superadmin@123", roleId: 9, status: true, empNo: 'OAC-2024-009' },
+        { name: "Dhanalakshmi", email: "dhanalakshmi@gmail.com", phoneNumber:"1234567890", password: "dhanalakshmi@123", roleId: 8, status: true, empNo: 'OAC-2024-008' },
         
     ];
 
