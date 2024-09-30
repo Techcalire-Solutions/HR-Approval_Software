d380c52521458eb0366cbe8d4650ab062143dc82
NishidaOAC <nishida@onboardaero.com>
2024-09-30 11:28:19 +0530

    error @ findkam

--- Backend/invoices/routers/performaInvoice.js
+++ Backend/invoices/routers/performaInvoice.js
@@ -450,11 +450,7 @@ router.get('/findbkam', authenticateToken, async(req, res) => {
 
 router.get('/findbyam', authenticateToken, async(req, res) => {
     let status = req.query.status;
-    console.log(status);
-    
     let user = req.user.id;
-    console.log(user);
-    
     
     let where = { amId: user };
 
@@ -463,7 +459,6 @@ router.get('/findbyam', authenticateToken, async(req, res) => {
     } else if (status === 'REJECTED') {
         where.status = { [Op.or]: ['KAM REJECTED', 'AM REJECTED'] };
     }
-    console.log(req.query.search);
     
     if (req.query.search !== '' && req.query.search !== 'undefined') {
         const searchTerm = req.query.search.replace(/\s+/g, '').trim().toLowerCase();
@@ -512,8 +507,6 @@ router.get('/findbyam', authenticateToken, async(req, res) => {
               count: totalCount,
               items: pi,
             };
-            console.log(response);
-            
             res.json(response);
           } else {
             res.send(pi);
