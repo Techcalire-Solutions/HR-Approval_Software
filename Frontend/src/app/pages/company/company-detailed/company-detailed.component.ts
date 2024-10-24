772d37366f9198e4a3b4212203a0d0b7b8185386
AminaOAC <amina@onboardaero.com>
2024-10-24 10:43:05 +0530

    company cleanig

--- Frontend/src/app/pages/company/company-detailed/company-detailed.component.ts
+++ Frontend/src/app/pages/company/company-detailed/company-detailed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { CompanyService } from '@services/company.service';
@@ -6,21 +6,22 @@ import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-company-detailed',
   standalone: true,
-  imports: [MatListModule, RouterModule, MatCardModule, MatIconModule, CommonModule],
+  imports: [MatListModule, RouterModule, MatCardModule, MatIconModule],
   templateUrl: './company-detailed.component.html',
   styleUrl: './company-detailed.component.scss'
 })
 export class CompanyDetailedComponent {
   shippingDetails: any;
   bankDetails: any;
-
-
-  constructor(private companyService:CompanyService, private route:ActivatedRoute, public dialog:MatDialog) { }
+  companyService=inject(CompanyService)
+  route=inject(ActivatedRoute)
+  dialog=inject(MatDialog)
+ 
 
   panelOpenState = false;
 
