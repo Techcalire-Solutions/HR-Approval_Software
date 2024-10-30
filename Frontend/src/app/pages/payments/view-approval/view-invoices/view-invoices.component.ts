7d064f2a4bf6eb723d97cd9b2cd8adeb867bc54b
AminaOAC <amina@onboardaero.com>
2024-10-30 08:57:39 +0530

    changes before meeting

--- Frontend/src/app/pages/payments/view-approval/view-invoices/view-invoices.component.ts
+++ Frontend/src/app/pages/payments/view-approval/view-invoices/view-invoices.component.ts
@@ -222,7 +222,7 @@ export class ViewInvoicesComponent {
     this.excelSub = this.invoiceService.excelExport(data).subscribe({
       next: (result: any) => {
         if (result && result.message === "Excel file saved successfully.") {
-          this.router.navigateByUrl('/login/viewApproval/viewexcel/view');
+          this.router.navigateByUrl('/login/viewApproval/viewexcel');
         } else {
           alert(result.message);
         }
