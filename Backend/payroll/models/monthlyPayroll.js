848a61045721323e9adf3ad86ea2be2aa8e9fe46
nishidaoac <nishida@onboardaero.com>
2025-08-19 09:01:48 +0530

    monthly payroll forced

--- Backend/payroll/models/monthlyPayroll.js
+++ Backend/payroll/models/monthlyPayroll.js
@@ -37,7 +37,7 @@ const MonthlyPayroll = sequelize.define('monthlyPayroll', {
   timestamps: true,
 });
 
-MonthlyPayroll.sync({ alter: true })
+MonthlyPayroll.sync({ force: true })
   .then(() => {
     console.log('Tables synced successfully.');
   })
