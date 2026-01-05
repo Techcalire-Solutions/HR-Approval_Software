3129be3cc5b1f4300cf4c6349e46056b1a526535
nishidaoac <nishida@onboardaero.com>
2026-01-05 09:59:54 +0530

    user

--- Frontend/src/app/pages/users/users.component.ts
+++ Frontend/src/app/pages/users/users.component.ts
@@ -24,7 +24,8 @@ import { SeparationComponent } from './separation/separation.component';
 import { User } from '../../common/interfaces/users/user';
 import { UpdateDesignationComponent } from './update-designation/update-designation.component';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
-import { RoleService } from '@services/role.service';
+import { LoginService } from '@services/login.service';
+import { InvoiceService } from '@services/invoice.service';
 
 
 @Component({
@@ -56,20 +57,24 @@ export class UsersComponent implements OnInit, OnDestroy {
   ngOnInit() {
     const token: any = localStorage.getItem('token')
     const user = JSON.parse(token)
-    const roleId = user.role
-    this.getRoleById(roleId)
-
+    const userId = user.id;
+    this.getUser(userId);
     this.getUsers()
   }
 
-  roleSub!: Subscription;
-  admin: boolean = false;
-  private roleService = inject(RoleService);
+  loginUserSub:Subscription
+  private loginService = inject(LoginService);
+  private invoiceService = inject(InvoiceService);
   role!: string;
-  getRoleById(id: number){
-    this.roleSub = this.roleService.getRoleById(id).subscribe(role => {
-      this.role = role.roleName; 
-    })
+  getUser(id: number) {
+    this.loginUserSub = this.loginService.getUserById(id).subscribe((res) => {
+      const user = res;
+      if (!user.userPosition || !user.userPosition.designation) {
+          this.role = 'Employee';
+      } else {
+          this.role = user.userPosition.designation.designationName;
+      }
+    });
   }
   
   userSub!: Subscription;
